af561d9ae388a795b10445cc037531701d9c9b51
theDrunkenManatee <samorege@hendrix.edu>
2020-04-16 14:08:17 -0500

    Update scriptFixerV2.py

--- scriptFixerV2.py
+++ scriptFixerV2.py
@@ -1,14 +1,21 @@
 def fixScriptText(filePath):
     splitLinesText = []
     with open(filePath) as file_in:
-        arrayToFix = makeScriptArray(file_in)
+        arrayToFix = rawScriptArray(file_in)
+        print(arrayToFix)
 
-    print(outString)
 
-def makeScriptArray(scriptFile):
+def rawScriptArray(scriptFile):
     scriptArray = []
     for line in scriptFile:
-
+        if len(line) == 1:
+            print("ping!")
+        else:
+            for word in line.split():
+                if word[len(word) - 1] == '\n':
+                    print("pong!")
+                else:
+                    scriptArray.append(word)
     return scriptArray
 
 
