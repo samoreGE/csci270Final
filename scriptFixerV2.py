6884317bd55a398c1e199ab1cdf4daf66dc5577e
theDrunkenManatee <samorege@hendrix.edu>
2020-04-16 15:45:07 -0500

    Update scriptFixerV2.py

--- scriptFixerV2.py
+++ scriptFixerV2.py
@@ -1,22 +1,53 @@
 def fixScriptText(filePath):
     splitLinesText = []
     with open(filePath) as file_in:
-        arrayToFix = rawScriptArray(file_in)
-        print(arrayToFix)
+        splitScript = makeSplitScript(file_in)
+        print(splitScript)
+        for line in splitScript:
+            print(line)
 
 
-def rawScriptArray(scriptFile):
+def makeSplitScript(scriptFile):
+    #CHANGE PER FILE
     scriptArray = []
     fullText = ""
     inChunk = True
     for line in scriptFile:
         fullText += line
-        for word in line.split():
-            if len(line) != 1:
-                if word[len(word) - 1] != '\n':
-                    scriptArray.append(word)
-    print(fullText.split("\n\n"))
+    for line in fullText.split("\n\n"):
+        scriptArray.append(line.replace('\n', ' '))
     return scriptArray
+
+
+def detectLineType(lineString):
+    #CHANGE PER FILE
+    
+
+
+def makeSettingLine(settingArray):
+    textOut = ["SETTING LINE: ["]
+    for word in settingArray:
+        textOut.append(word)
+    textOut.append("] END SETTNG")
+    return textOut
+
+
+def makeStageDirection(wordArray):
+    textOut = ["STAGE LINE ("]
+    for word in wordArray:
+        textOut.append(word)
+    textOut.append(") END STAGE")
+    return textOut
+
+
+def makeCharLine(nameArray, lineArray):
+    textOut = []
+    for word in nameArray:
+        textOut.append(word)
+    textOut.append(":")
+    for word in lineArray:
+        textOut.append(word)
+    return textOut
 
 
 fixScriptText("Scripts/Raw Scripts/TheDoormanRaw.txt")
