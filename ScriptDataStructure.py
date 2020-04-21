b3ffe61454aa1cc33ba4ce4ebfcd2b1791eb48b8
theDrunkenManatee <samorege@hendrix.edu>
2020-04-21 11:32:43 -0500

    made the data structure!

--- ScriptDataStructure.py
+++ ScriptDataStructure.py
@@ -1,6 +1,56 @@
+from abc import ABC, abstractmethod
 
-class DialogueLine:
-    character = ""
 
-    def doStuff(self):
-        return self.character
+class SceneLine(ABC):
+
+    @abstractmethod
+    def getText(self):
+        pass
+
+
+class LineText(ABC):
+    value = ""
+
+    @abstractmethod
+    def getValue(self):
+        pass
+
+
+class Episode:
+    def __init__(self, scenes):
+        self.scenes = scenes
+
+
+class Scene:
+    def __init__(self, setting, characters, lines):
+        self.setting = setting
+        self.characters = characters
+        self.lines = lines
+
+
+class DialogueLine(SceneLine):
+    def __init__(self, speaker, lineTextArray):
+        self.speaker = speaker
+        self.lineTextArray = lineTextArray
+
+    def getText(self):
+        return self.lineTextArray
+
+
+class StageDir(SceneLine, LineText):
+    def __init__(self, dirText):
+        self.dirText = dirText
+
+    def getText(self):
+        return self.dirText
+
+    def getValue(self):
+        return self.dirText
+
+
+class DialogueWord(LineText):
+    def __init__(self, wordText):
+        self.wordText = wordText
+
+    def getValue(self):
+        return self.wordText
