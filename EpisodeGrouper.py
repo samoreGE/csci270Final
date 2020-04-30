520740bb2cd37e2d9370085a10d24c6d82b4a4cb
theDrunkenManatee <samorege@hendrix.edu>
2020-04-29 21:53:41 -0500

    Update EpisodeGrouper.py

--- EpisodeGrouper.py
+++ EpisodeGrouper.py
@@ -1,5 +1,12 @@
 from ScriptDataStructure import *
 from ScriptParser import *
 
+
 def makeDemo(epNames, character):
+    episodes = []
     for name in epNames:
+        newEpisodeObject = scriptToEpisodeObj("Scripts/Normalized Scripts/" + name + ".txt", name)
+        episodes.append(newEpisodeObject)
+
+
+makeDemo()
