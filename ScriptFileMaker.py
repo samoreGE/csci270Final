b1a84bc840b77ce63b6fefacd4587cdd06ce4986
theDrunkenManatee <samorege@hendrix.edu>
2020-04-30 01:01:17 -0500

    file creation is a thing!

--- ScriptFileMaker.py
+++ ScriptFileMaker.py
@@ -1,3 +1,21 @@
 from EpisodeGrouper import *
-f = open("guru99.txt", "w+")
-checkResults(getAllCharLines(demoNames, "george"), "george")
+
+
+def makeScriptFiles(epNames, characters):
+    for character in characters:
+        makeScriptFileForChar(epNames, character)
+
+
+def makeScriptFileForChar(epNames, character):
+    f = open("Data/" + character + "Lines.txt", "w+")
+    for line in getAllCharLines(epNames, character):
+        lineToAdd = ""
+        for word in line.getChainableSource():
+            lineToAdd += " " + word + " "
+        f.write(lineToAdd[1:-1] + "\n")
+
+
+demoNames = ["MaleUnbonding", "TheDoorman", "TheExGirlfriend", "TheJacket", "ThePonyRemark", "TheStockTip"]
+charNames = ["jerry", "kramer", "elaine", "george"]
+
+makeScriptFiles(demoNames, charNames)
