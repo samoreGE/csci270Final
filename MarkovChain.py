0c548a7e004348b594ad34be9ac536d42a3470b1
theDrunkenManatee <samorege@hendrix.edu>
2020-04-30 10:03:28 -0500

    Update MarkovChain.py

--- MarkovChain.py
+++ MarkovChain.py
@@ -9,32 +9,36 @@ class MarkovChain:
     def addData(self, newDataArray):
         print("adding data from " + str(newDataArray))
         if len(newDataArray) > self.order:
+            nodeKey = "DATASTART"
             for index in range(len(newDataArray) - self.order + 1):
-                print("index=", index)
-                self.addToNode(self.makeKey(newDataArray, index))
-
+                newKey = self.makeKey(newDataArray, index)
+                self.addToNode(nodeKey, newKey)
+                nodeKey = newKey
+            self.addToNode(nodeKey, "DATAEND")
         else:
             print("ERROR: DATA < ORDER")
 
     def makeKey(self, source, index):
-        print("Making key from " + str(source))
         outKey = ""
         for loc in range(self.order):
             outKey += source[loc + index] + " "
         return outKey
 
-    def addToNode(self, nodeKey):
-        print("adding to nodeKey " + nodeKey)
-        if nodeKey in self.nodes:
-            self.nodes[nodeKey] += 1
+    def addToNode(self, nodeKey, linkKey):
+        print("nodeKey: " + nodeKey + ", linkKey: " + linkKey)
+        if linkKey in self.getNode(nodeKey):
+            self.getNode(nodeKey)[linkKey] += 1
         else:
-            self.nodes[nodeKey] = 1
-        print("new total: ")
+            self.getNode(nodeKey)[linkKey] = 1
+        print("new total: ", self.getNode(nodeKey)[linkKey])
+
+    def getNode(self, nodeKey):
+        if nodeKey not in self.nodes:
+            self.nodes[nodeKey] = dict()
+        return self.nodes[nodeKey]
 
 
-
-
-demoChain = MarkovChain(2)
+demoChain = MarkovChain(1)
 demoNames = ["MaleUnbonding", "TheDoorman", "TheExGirlfriend", "TheJacket", "ThePonyRemark", "TheStockTip"]
 jerryLines = getAllCharLines(demoNames, "jerry")
 for line in jerryLines:
