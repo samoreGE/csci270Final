21204370fe84049476162a3b4fff070dd70eb0d8
theDrunkenManatee <samorege@hendrix.edu>
2020-04-29 22:32:09 -0500

    more pushing

--- EpisodeGrouper.py
+++ EpisodeGrouper.py
@@ -3,11 +3,31 @@ from ScriptParser import *
 
 
 def makeDemo(epNames, character):
+    episodes = getEpisodes(epNames)
+    for episode in episodes:
+        getCharLines(episodes, character)
+
+def getEpisodes(epNames):
     episodes = []
     for name in epNames:
         newEpisodeObject = scriptToEpisodeObj("Scripts/Normalized Scripts/" + name + "Fixed.txt", name)
-        episodes.append(newEpisodeObject)
+        if isinstance(newEpisodeObject, Episode):
+            episodes.append(newEpisodeObject)
+        else:
+            print("ERROR: NEW EPISODE NOT MADE")
+    return episodes
+
+def getCharLines(episode, character):
+    for scene in episode.scenes:
+        if isinstance(scene, Scene):
+            for line in scene:
+                if isinstance(line, DialogueLine):
+                    
+                elif not isinstance(line, StageDir):
+                    print("ERROR: NEW EPISODE NOT MADE")
+        else:
+            print("ERROR: NON-SCENE FOUND IN EPISODE")
 
 
-demoNames = ["MaleUnbonding", "TheDoorman", "TheExGirlfriend"]
+demoNames = ["MaleUnbonding", "TheDoorman", "TheExGirlfriend", "TheJacket", "ThePonyRemark", "TheStockTip"]
 makeDemo(demoNames, "jerry")
