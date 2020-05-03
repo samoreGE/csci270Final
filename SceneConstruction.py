bcced57f06126d3dd9821ba863fb3faa26461c06
Jack Frey <johnjackfrey@gmail.com>
2020-05-03 17:32:30 -0500

    Getting Speaker Sequences

--- SceneConstruction.py
+++ SceneConstruction.py
@@ -4,7 +4,7 @@ from EpisodeGrouper import *
 class SceneBuilder:
     def __init__(self, episodes):
         self.episodes = episodes #Should be a list of all episode names
-        self.chain = MarkovChain(2)
+        self.lineChain = MarkovChain(2)
         self.sceneList = self.readScenes()
 
     def readScenes(self):
@@ -15,4 +15,14 @@ class SceneBuilder:
                 scenes.append(s)
         return scenes
 
+    def lineChainBuilder(self):
+        for s in self.sceneList:
+            for l in range(len(s.lines) - 1):
+                if s.lines[l].type == "line":
+                    speaker = s.lines[l].getSceneLineText().split(":")[0]
+                    print(speaker)
+
+
 test = SceneBuilder(["MaleUnbonding", "TheDoorman", "TheExGirlfriend", "TheJacket", "ThePonyRemark", "TheStockTip"])
+print("-------------------------------------------------------------------")
+test.lineChainBuilder()
