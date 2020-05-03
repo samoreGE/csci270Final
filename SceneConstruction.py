e1edd06122486f20a9330b414ac0f3eb121990c8
Jack Frey <johnjackfrey@gmail.com>
2020-05-03 18:34:15 -0500

    Testing with temp chain

--- SceneConstruction.py
+++ SceneConstruction.py
@@ -4,7 +4,8 @@ from EpisodeGrouper import *
 class SceneBuilder:
     def __init__(self, episodes):
         self.episodes = episodes #Should be a list of all episode names
-        self.lineChain = MarkovChain(2)
+        self.tempChain = {}
+        self.tempKeys = set({})
         self.sceneList = self.readScenes()
 
     def readScenes(self):
@@ -17,12 +18,25 @@ class SceneBuilder:
 
     def lineChainBuilder(self):
         for s in self.sceneList:
-            for l in range(len(s.lines) - 1):
+            for l in range(len(s.lines) - 2):
                 if s.lines[l].type == "line":
-                    speaker = s.lines[l].getSceneLineText().split(":")[0]
-                    print(speaker)
-
+                    speaker1 = s.lines[l].getSceneLineText().split(":")[0]
+                else:
+                    speaker1 = "STAGEDIR"
+                if s.lines[l + 1].type == "line":
+                    speaker2 = s.lines[l + 1].getSceneLineText().split(":")[0]
+                else:
+                    speaker2 = "STAGEDIR"
+                if s.lines[l + 2].type == "line":
+                    speaker3 = s.lines[l + 2].getSceneLineText().split(":")[0]
+                else:
+                    speaker3 = "STAGEDIR"
+                entry = (speaker3, speaker2, speaker1) #ordered most recent to least recent
+                if entry not in self.tempKeys:
+                    self.tempKeys.add(entry)
+                    self.tempChain[entry] = 0
+                self.tempChain[entry] += 1
 
 test = SceneBuilder(["MaleUnbonding", "TheDoorman", "TheExGirlfriend", "TheJacket", "ThePonyRemark", "TheStockTip"])
-print("-------------------------------------------------------------------")
 test.lineChainBuilder()
+print(test.tempChain[('elaine', 'jerry', 'elaine')])
